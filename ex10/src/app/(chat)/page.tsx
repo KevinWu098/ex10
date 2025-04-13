03c7c83977e04aea6e5769fc3ca16f5a6039db64
Kevin Wu <kevinwu098@gmail.com>
2025-04-12 20:01:42 -0700

    feat: sidebar

---
+++ ex10/src/app/(chat)/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <div className="flex items-center justify-center">
+            <h1 className="text-2xl font-semibold">
+                What browser extension will you create?
+            </h1>
+        </div>
+    );
+}
